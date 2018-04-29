7556d3a9aa0387270b7ef9820df96f79f2f3de90
shakti_sd <shakti.sd@gmail.com>
2018-04-29 15:56:25 +0530

    Refactored code to use all_articles1.csv

--- demo/seq2seq_glove_v2_train.py
+++ demo/seq2seq_glove_v2_train.py
@@ -18,7 +18,7 @@ def main():
     model_dir_path = './models'
 
     print('loading csv file ...')
-    df = pd.read_csv(data_dir_path + "/fake_or_real_news.csv")
+    df = pd.read_csv(data_dir_path + "/all_article1.csv")
 
     print('extract configuration from input texts ...')
     Y = df.title
